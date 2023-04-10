9b3ad7dd450ebe5931e1c7b3e2e816c6354aba87
crazyuchoxs <internal.777.iky@gmail.com>
2023-04-10 21:49:22 +0700

    tailwind

---
+++ public/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index2.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "header-image": "url('src/Background01.png')",
+      }
+    },
+  },
+  plugins: [],
+}
+
